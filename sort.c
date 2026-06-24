ebbaf9ee7ade77a4556237f68b243dd477d9343c
Daniil <the.sadichh@gmail.com>
2026-06-24 15:48:42 +0300

    Добавлен алгоритм Сортировки Шелла

---
+++ sort.c
@@ -0,0 +1,18 @@
+﻿#include "sort.h"
+
+void shell_sort(int* arr, int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j = i;
+            
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            
+            arr[j] = temp;
+        }
+    }
+}
